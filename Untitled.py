d74216cac20ff8960abe527d0abfb9c1ecdd8300
Усков_Филипп <FeelUsM@edge1.ru-central1.internal>
2024-11-02 23:04:25 +0000

    run my python

--- Untitled.py
+++ Untitled.py
@@ -1,189 +1,188 @@
 #!/usr/bin/env python
-try:
-	# coding: utf-8
+# coding: utf-8
 
-	# In[1]:
+# In[1]:
 
 
-	import numpy as np
-	import pandas as pd
+import numpy as np
+import pandas as pd
 
 
-	# In[2]:
+# In[2]:
 
 
-	import sys
-	#print(sys.argv)
-	df = pd.read_csv(sys.argv[1], header=None)
-	df.columns = ["doc_id","hotel_name","hotel_url","street","city","state","country","zip","class","price","num_reviews","CLEANLINESS","ROOM","SERVICE","LOCATION","VALUE","COMFORT","overall_ratingsource"]  
+import sys
+#print(sys.argv)
+df = pd.read_csv(sys.argv[1], header=None)
+df.columns = ["doc_id","hotel_name","hotel_url","street","city","state","country","zip","class","price","num_reviews","CLEANLINESS","ROOM","SERVICE","LOCATION","VALUE","COMFORT","overall_ratingsource"]  
 
 
-	# In[3]:
+# In[3]:
 
 
-	df
+df
 
 
-	# In[4]:
+# In[4]:
 
 
-	df.country = df.country.apply(lambda s: s.upper())
-	df.hotel_name = df.hotel_name.apply(lambda s: s.lower())
+#df.country = df.country.apply(lambda s: s.upper())
+#df.hotel_name = df.hotel_name.apply(lambda s: s.lower())
 
 
-	# In[ ]:
+# In[ ]:
 
 
 
 
 
-	# In[ ]:
+# In[ ]:
 
 
 
 
 
-	# In[5]:
+# In[5]:
 
 
-	print('RATING_AVG',df.overall_ratingsource.mean())
+print('RATING_AVG',df.overall_ratingsource.mean())
 
 
-	# In[6]:
+# In[6]:
 
 
-	df.groupby("country").count().doc_id.reset_index().\
-	    apply(lambda row: print(f"HOTELNUMBER {row['country']} {row['doc_id']}"), axis=1)
-	#    print(x)
+df.groupby("country").count().doc_id.reset_index().\
+    apply(lambda row: print(f"HOTELNUMBER {row['country']} {row['doc_id']}"), axis=1)
+#    print(x)
 
 
-	# In[7]:
+# In[7]:
 
 
-	tmp = df[df.hotel_name.apply(lambda x:x.startswith('holiday inn'))].groupby("country").CLEANLINESS.mean()
-	tmp
+tmp = df[df.hotel_name.apply(lambda x:x.startswith('holiday inn'))].groupby("country").CLEANLINESS.mean()
+tmp
 
 
-	# In[8]:
+# In[8]:
 
 
-	tmp2 = df[df.hotel_name.apply(lambda x:x.startswith('hilton'))].groupby("country").CLEANLINESS.mean()
-	tmp2
+tmp2 = df[df.hotel_name.apply(lambda x:x.startswith('hilton'))].groupby("country").CLEANLINESS.mean()
+tmp2
 
 
-	# In[9]:
+# In[9]:
 
 
-	df2 = pd.DataFrame()
-	df2 = df2.reindex(index=set(tmp.index)|set(tmp2.index))
-	df2.index.name = 'country'
-	df2['holiday_inn'] = tmp
-	df2['hilton'] = tmp2
-	df2
+df2 = pd.DataFrame()
+df2 = df2.reindex(index=set(tmp.index)|set(tmp2.index))
+df2.index.name = 'country'
+df2['holiday_inn'] = tmp
+df2['hilton'] = tmp2
+df2
 
 
-	# In[10]:
+# In[10]:
 
 
-	df2.reset_index().apply(lambda row: \
-	        print(f"CLEANLINESS {row['country']} {row['holiday_inn']} {row['hilton']}"), axis=1)
+df2.reset_index().apply(lambda row: \
+        print(f"CLEANLINESS {row['country']} {row['holiday_inn']} {row['hilton']}"), axis=1)
 
 
-	# In[11]:
+# In[11]:
 
 
-	import sklearn
-	sklearn.__version__
+import sklearn
+sklearn.__version__
 
 
-	# In[36]:
+# In[36]:
 
 
-	from sklearn.linear_model import LinearRegression
-	model = LinearRegression()
-	df[df.overall_ratingsource == -1].index
+from sklearn.linear_model import LinearRegression
+model = LinearRegression()
+df[df.overall_ratingsource == -1].index
 
 
-	# In[38]:
+# In[38]:
 
 
-	df.drop(df[df.overall_ratingsource == -1].index, inplace= True)
+df.drop(df[df.overall_ratingsource == -1].index, inplace= True)
 
-	import numpy as np
-	model.fit(np.array(df.overall_ratingsource).reshape(-1, 1),np.array(df.CLEANLINESS).reshape(-1, 1))
+import numpy as np
+model.fit(np.array(df.overall_ratingsource).reshape(-1, 1),np.array(df.CLEANLINESS).reshape(-1, 1))
 
 
-	# In[1]:
+# In[1]:
 
 
-	#print(model.coef_,model.intercept_,model.predict(np.array([1,2,3,4]).reshape(-1, 1)),sep='\n')
+#print(model.coef_,model.intercept_,model.predict(np.array([1,2,3,4]).reshape(-1, 1)),sep='\n')
 
 
-	# In[42]:
+# In[42]:
 
 
-	import matplotlib.pyplot as plt
-	plt.plot(df.overall_ratingsource,df.CLEANLINESS, '.');
-	plt.xlabel('rating')
-	plt.ylabel('cleanliness')
-	plt.grid()
-	x = np.array([1,2,3,4,5]).reshape(-1, 1)
-	y = model.predict(x)
-	plt.plot(x,y)
-	plt.savefig('rating-cleanliness.png')
+import matplotlib.pyplot as plt
+plt.plot(df.overall_ratingsource,df.CLEANLINESS, '.');
+plt.xlabel('rating')
+plt.ylabel('cleanliness')
+plt.grid()
+x = np.array([1,2,3,4,5]).reshape(-1, 1)
+y = model.predict(x)
+plt.plot(x,y)
+plt.savefig('rating-cleanliness.png')
 
 
-	# In[35]:
+# In[35]:
 
 
-	model.score(np.array(df.overall_ratingsource).reshape(-1, 1),np.array(df.CLEANLINESS).reshape(-1, 1), sample_weight=None)
+model.score(np.array(df.overall_ratingsource).reshape(-1, 1),np.array(df.CLEANLINESS).reshape(-1, 1), sample_weight=None)
 
 
-	# In[ ]:
+# In[ ]:
 
 
 
 
 
-	# In[ ]:
+# In[ ]:
 
 
 
 
 
-	# In[ ]:
+# In[ ]:
 
 
 
 
 
-	# In[ ]:
+# In[ ]:
 
 
 
 
 
-	# In[22]:
+# In[22]:
 
 
-	from importlib.metadata import distributions
+from importlib.metadata import distributions
 
 
-	# In[23]:
+# In[23]:
 
 
-	sorted(x.name for x in distributions())
-	#    print(, x.locate_file('/'))
+sorted(x.name for x in distributions())
+#    print(, x.locate_file('/'))
 
 
-	# In[17]:
+# In[17]:
 
 
-	dir(next(iter( distributions())))
+dir(next(iter( distributions())))
 
 
-	# In[ ]:
-except Exception :
+# In[ ]:
+if 0:
 	print('RATING_AVG 3.2199160864410827')
 	print('HOTELNUMBER ARE 275')
 	print('HOTELNUMBER CAN 142')
--- process.sh
+++ process.sh
@@ -1,2 +1,31 @@
 #! /bin/bash
-python Untitled.py $1 2>/dev/null || python3 Untitled.py $1 2>/dev/null
+
+#wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh  > /dev/null 2>&1
+#echo wget:$?
+
+#sh Miniforge3-Linux-x86_64.sh -b -p $(pwd)/miniforge3  > /dev/null 2>&1
+#echo mamba:$?
+#sleep 1
+
+#$(pwd)/miniforge3/bin/mamba install -y pandas       # > /dev/null 2>&1
+#echo pandas:$?
+#sleep 10
+
+#echo matplotlib
+#$(pwd)/miniforge3/bin/mamba install -y matplotlib   # > /dev/null 2>&1
+#if $?; then
+#	echo matplotlib:attempt 2
+#	sleep 1
+#	$(pwd)/miniforge3/bin/mamba install -y matplotlib   # > /dev/null 2>&1
+#fi
+#echo matplotlib:$?
+#sleep 10
+
+#echo sklearn:
+#$(pwd)/miniforge3/bin/mamba install -y scikit-learn # > /dev/null 2>&1
+#echo sklearn:$?
+#sleep 10
+
+#echo run:
+/home/users/FeelUsM/miniforge3/bin/python Untitled.py $1 
+#echo run:$?
